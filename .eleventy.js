abaf8caa8b78552f0fa1588299c7d722d673f371
Brett Roper <roper.brett@gmail.com>
2023-07-21 13:20:45 +0800

    Basic styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
-module.exports = function(eleventyConfig){
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
+    eleventyConfig.addPassthroughCopy("src/assets/images");
+  
     return {
       dir: {
         input: "src",
         data: "_data",
         includes: "_includes",
-        layouts: "_layouts"
-      }
+        layouts: "_layouts",
+      },
     };
-  }
+  };
--- src/index.md
+++ src/index.md
@@ -3,6 +3,8 @@ layout: base
 title: Home
 ---
 
+![hero image](assets/images/sugarloaf-sunset.jpg)
+
 # Brett Roper
 
 Stuff to remember.
